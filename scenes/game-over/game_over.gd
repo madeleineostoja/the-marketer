52a92e2ccfe176193b268ab255049b874434eea2
Madeleine Ostoja <madi@madeleineostoja.com>
2023-12-06 23:26:23 +1300

    Update game over UI

--- scenes/game-over/game_over.gd
+++ scenes/game-over/game_over.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var global = get_node('/root/Global')
 
 func _ready():
-	$Score.text = str(global.score)
+	$ScoreValue.text = str(global.score)
 	$Restart.grab_focus()
 
 func _on_restart_pressed(): 
